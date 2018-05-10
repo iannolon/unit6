87ce8ce7048b5943751ff1e32211388e7038d138
iannolon <35769152+iannolon@users.noreply.github.com>
2018-05-10 08:02:47 -0400

    Create fileDemo.py

---
+++ fileDemo.py
@@ -0,0 +1,13 @@
+#IanNolon
+#5/9/18
+#fileDemo.py - how to read a file
+
+file = open('engmix.txt')
+
+numWords = 0
+for line in file:
+    if 'nolon' in line:
+        print(line.strip())
+    numWords += 1
+    
+print(numWords)
