e923083f98c534ffb259ff56e1855082798f10a5
iannolon <35769152+iannolon@users.noreply.github.com>
2018-05-10 08:41:47 -0400

    Updated from Brython Server: 5/10/2018 8:41:46 AM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,11 @@
+#IanNolon
+#5/10/18
+#howManyWords.py
 
+file = open('engmix.txt')
+
+nums = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+for word in file:
+    nums[len(word.strip())] += 1
+print(nums)
