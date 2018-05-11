fb6385bf4f624788054bcb959de767d758b2361c
iannolon <35769152+iannolon@users.noreply.github.com>
2018-05-11 08:37:11 -0400

    Updated from Brython Server: 5/11/2018 8:37:11 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,14 @@
+#IanNolon
+#5/11/18
+#warmup16.py
 
+file = open('engmix.txt')
+
+count = 0
+for line in file:
+    line = line.strip()
+    if len(line) > 0:
+        if line[0] == 'i' and line[-1] == 'n':
+            print(line)
+            count += 1
+print(count)
