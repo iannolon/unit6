db681a5bbe78a9a29acf0a0a20309470270b662a
iannolon <35769152+iannolon@users.noreply.github.com>
2018-05-14 08:11:28 -0400

    Updated from Brython Server: 5/14/2018 8:11:27 AM

--- homework2.py
+++ homework2.py
@@ -1,1 +1,12 @@
+#IanNolon
+#5/14/18
+#homework2
 
+file = open('engmix.txt')
+
+dictionary = []
+for line in file:
+    line = line.strip()
+    dictionary.append(line)
+num = int(input('Enter a number: '))
+print(dictionary[num-1])
