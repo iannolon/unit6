c6217762b857089d9c102c85baf046f573fdfed4
iannolon <35769152+iannolon@users.noreply.github.com>
2018-05-14 08:36:53 -0400

    Updated from Brython Server: 5/14/2018 8:36:52 AM

--- homework4.py
+++ homework4.py
@@ -1,1 +1,15 @@
+#IanNolon
+#5/14/18
+#homework4
 
+file = open('engmix.txt')
+
+letter = input('Enter a letter: ')
+word = 0
+numLetters = 0
+for line in file:
+    line = line.strip()
+    if line.count(letter) > numLetters:
+        word = line
+        numLetters = line.count(letter)
+print(word,'has',numLetters,'occurences of the letter',letter)
