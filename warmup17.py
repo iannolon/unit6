d666a8ddfe15a5f7ad3b9e8f65f91b8f72c158e4
iannolon <35769152+iannolon@users.noreply.github.com>
2018-05-17 08:20:38 -0400

    Updated from Brython Server: 5/17/2018 8:20:38 AM

--- warmup17.py
+++ warmup17.py
@@ -1,1 +1,10 @@
+#IanNolon
+#5/17/18
+#warmup17.py
 
+file = open('engmix.txt')
+
+for line in file:
+    line = line.strip()
+    if 'l' in line and 'n' in line and 'i' in line and 'o' in line and 'a' in line:
+        print(line)
